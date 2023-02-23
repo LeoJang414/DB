4e29e6e48d5a0cc5fedf5d8f9767612a707b7682
LeoJang414 <124111029+LeoJang414@users.noreply.github.com>
2023-02-23 17:40:03 +0900

    DB01

---
+++ Script-1.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE member2 (
+	id varchar2(100),
+	pw varchar2(100),
+	name varchar2(100),
+	tel varchar2(100)
+	)
+	
+
+	
+INSERT INTO "member" VALUES ('park','1234','park','011')
+
+INSERT INTO "member" VALUES ('apple','1111','apple','011')
+
+INSERT INTO "member" VALUES ('ice','2222','ice','012')
+
+INSERT INTO "member" VALUES ('melon','3333','melon','013')
+
+SELECT * FROM "member"
+
+SELECT id FROM "member"
+
+SELECT id, pw FROM "member"
